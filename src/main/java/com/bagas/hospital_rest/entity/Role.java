2c4f951036c611b79c680f9aa26e3f0c8b87209c
Alexandr Bogoslovskiy <balics@mail.ru>
2024-03-23 09:35:45 +0300

    link many to many relationship

--- src/main/java/com/bagas/hospital_rest/entity/Role.java
+++ src/main/java/com/bagas/hospital_rest/entity/Role.java
@@ -1,9 +1,12 @@
 package com.bagas.hospital_rest.entity;
 
+import java.util.Set;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -17,4 +20,7 @@ public class Role {
 	private long id;
 	
 	private String authority;
+	
+	@ManyToMany(mappedBy = "authorities")
+	private Set<UserEntity> users;
 }
