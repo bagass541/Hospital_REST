ffe5a84814182c8f3d9ce6eda7674f9a9f66693e
Alexandr Bogoslovskiy <balics@mail.ru>
2024-03-23 12:10:16 +0300

    add new methods to get by id, to delete, to change

--- src/main/java/com/bagas/hospital_rest/controller/DoctorController.java
+++ src/main/java/com/bagas/hospital_rest/controller/DoctorController.java
@@ -4,12 +4,19 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bagas.hospital_rest.entity.DoctorEntity;
+import com.bagas.hospital_rest.exceptions.DoctorNotFoundException;
 import com.bagas.hospital_rest.models.Doctor;
 import com.bagas.hospital_rest.services.DoctorService;
 
@@ -21,7 +28,7 @@ public class DoctorController {
 	private DoctorService doctorService;
 
 	@PostMapping
-	public ResponseEntity<String> createDoctor(DoctorEntity doctor) {
+	public ResponseEntity<String> createDoctor(@RequestBody DoctorEntity doctor) {
 		DoctorEntity newDoctor = doctorService.createDoctor(doctor);
 		
 		if(newDoctor != null) {
@@ -37,5 +44,22 @@ public class DoctorController {
 		List<Doctor> doctors = doctorService.getAll();
 		
 		return ResponseEntity.ok(doctors);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Doctor> getOneDoctor(@PathVariable("id") Long id) throws DoctorNotFoundException {
+		System.out.println(id);
+		return ResponseEntity.ok(doctorService.getOne(id));
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Long> deleteDoctor(@PathVariable("id") Long id) {
+		doctorService.delete(id);
+		return ResponseEntity.ok(id);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Doctor> changeDoctor(@PathVariable("id") Long id, @RequestBody DoctorEntity doctor) throws DoctorNotFoundException {
+		return ResponseEntity.ok(doctorService.updateDoctor(id, doctor));
 	}
 }
--- src/main/java/com/bagas/hospital_rest/services/DoctorService.java
+++ src/main/java/com/bagas/hospital_rest/services/DoctorService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.bagas.hospital_rest.entity.DoctorEntity;
+import com.bagas.hospital_rest.entity.DoctorType;
 import com.bagas.hospital_rest.exceptions.DoctorNotFoundException;
 import com.bagas.hospital_rest.models.Doctor;
 import com.bagas.hospital_rest.repositories.DoctorRepo;
@@ -26,9 +27,31 @@ public class DoctorService {
 	}
 	
 	public Doctor getOne(Long id) throws DoctorNotFoundException {
-		DoctorEntity doctor = doctorRepo.findById(id).get();
+		DoctorEntity doctor = doctorRepo.findById(id)
+				.orElseThrow(() -> new DoctorNotFoundException());
+
 		if(doctor == null) throw new DoctorNotFoundException();
-		
+
 		return Doctor.toModel(doctor);
+	}
+	
+	public Long delete(Long id) {
+		doctorRepo.deleteById(id);
+		return id;
+	}
+	
+	public Doctor updateDoctor(Long id, DoctorEntity doctor) throws DoctorNotFoundException {
+		DoctorEntity updatingDoctor = doctorRepo.findById(id)
+				.orElseThrow(() -> new DoctorNotFoundException());
+		
+		updatingDoctor.setId(doctor.getId());
+		updatingDoctor.setFio(doctor.getFio());
+		updatingDoctor.setDoctorType(doctor.getDoctorType());
+		updatingDoctor.setStartWork(doctor.getStartWork());
+		updatingDoctor.setEndWork(doctor.getEndWork());
+		
+		doctorRepo.save(updatingDoctor);
+		
+		return Doctor.toModel(updatingDoctor);
 	}
 }
