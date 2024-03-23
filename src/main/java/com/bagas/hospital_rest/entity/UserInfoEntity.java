310bf8c6e5f5ade17602ff51da65caf81f1a747b
Alexandr Bogoslovskiy <balics@mail.ru>
2024-03-23 09:29:58 +0300

    add new entity

---
+++ src/main/java/com/bagas/hospital_rest/entity/UserInfoEntity.java
@@ -0,0 +1,28 @@
+package com.bagas.hospital_rest.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Entity
+@Table(name = "users_info")
+@Data
+@EqualsAndHashCode(of = "id")
+public class UserInfoEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	private String fio;
+	
+	private String number;
+	
+	@OneToOne(mappedBy = "userInfoEntity")
+	private UserEntity userEntity;
+}
