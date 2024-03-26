e48da873d32711ce461afa71e5aea9c20264805b
Alexandr Bogoslovskiy <balics@mail.ru>
2024-03-26 22:23:50 +0300

    add links to model

--- src/main/java/com/bagas/hospital_rest/models/Doctor.java
+++ src/main/java/com/bagas/hospital_rest/models/Doctor.java
@@ -4,13 +4,20 @@ import java.time.LocalTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.RepresentationModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+
+import com.bagas.hospital_rest.controller.AppointmentController;
+import com.bagas.hospital_rest.controller.DoctorController;
 import com.bagas.hospital_rest.entity.DoctorEntity;
 import com.bagas.hospital_rest.entity.DoctorType;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.Data;
 
 @Data
-public class Doctor {
+public class Doctor extends RepresentationModel<Doctor> {
 	
 	private long id;
 	
@@ -22,9 +29,14 @@ public class Doctor {
 	
 	private LocalTime endWork;
 	
+	@JsonIgnore
 	private List<DoctorAppointment> appointments;
 	
-	public static Doctor toModel(DoctorEntity entity) {
+	public static Doctor toModel(DoctorEntity entity)   {
+		if(entity == null) {
+			return null;
+		}
+		
 		Doctor doctor = new Doctor();
 		doctor.setId(entity.getId());
 		doctor.setDoctorType(entity.getDoctorType());
@@ -33,6 +45,13 @@ public class Doctor {
 		doctor.setEndWork(entity.getEndWork());
 		doctor.setAppointments(entity.getAppointments().stream()
 				.map(DoctorAppointment::toModel).collect(Collectors.toList()));
+		
+		Link selfLink = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(DoctorController.class)
+				.getOneDoctor(doctor.getId())).withSelfRel();
+		Link appointments = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(AppointmentController.class)
+				.getAppointmentsForDoctor(doctor.getId())).withRel("appointments");
+				
+		doctor.add(selfLink, appointments);
 		
 		return doctor;
 	}
