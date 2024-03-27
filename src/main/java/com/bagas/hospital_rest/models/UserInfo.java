12476b2208f15dba88e92f5a123d0a0216525710
Alexandr Bogoslovskiy <balics@mail.ru>
2024-03-27 20:58:46 +0300

    delete creation links from controller and add it to model class

--- src/main/java/com/bagas/hospital_rest/models/UserInfo.java
+++ src/main/java/com/bagas/hospital_rest/models/UserInfo.java
@@ -1,12 +1,17 @@
 package com.bagas.hospital_rest.models;
 
+import org.springframework.hateoas.Link;
 import org.springframework.hateoas.RepresentationModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 
+import com.bagas.hospital_rest.controller.UserController;
 import com.bagas.hospital_rest.entity.UserInfoEntity;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
+@EqualsAndHashCode(callSuper = false)
 public class UserInfo extends RepresentationModel<UserInfo>{
 	
 	private long id;
@@ -20,6 +25,11 @@ public class UserInfo extends RepresentationModel<UserInfo>{
 		userInfo.setId(entity.getId());
 		userInfo.setFio(entity.getFio());
 		userInfo.setNumber(entity.getNumber());
+		
+		Link userLink = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(UserController.class)
+				.getOneUser(entity.getUserEntity().getId())).withRel("user");
+		
+		userInfo.add(userLink);
 		
 		return userInfo;
 	}
