f843145940912ee43f0894c9032ee055e669b892
Alexandr Bogoslovskiy <balics@mail.ru>
2024-03-27 20:57:21 +0300

    delete creation links from controller and add it to model class

--- src/main/java/com/bagas/hospital_rest/models/Role.java
+++ src/main/java/com/bagas/hospital_rest/models/Role.java
@@ -1,7 +1,10 @@
 package com.bagas.hospital_rest.models;
 
+import org.springframework.hateoas.Link;
 import org.springframework.hateoas.RepresentationModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 
+import com.bagas.hospital_rest.controller.UserController;
 import com.bagas.hospital_rest.entity.RoleEntity;
 
 import lombok.Data;
@@ -17,6 +20,11 @@ public class Role extends RepresentationModel<Role>{
 		Role role = new Role();
 		role.setId(entity.getId());
 		role.setAuthority(entity.getAuthority());
+		
+		Link users = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(UserController.class)
+				.getUsers()).withRel("users");
+		
+		role.add(users);
 		
 		return role;
 	}
