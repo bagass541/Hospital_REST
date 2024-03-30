0ac2792f79b287cb4d938f38348e8a05b04d2e91
Alexandr Bogoslovskiy <balics@mail.ru>
2024-03-30 10:17:10 +0300

    override findAll method with fetch joins

--- src/main/java/com/bagas/hospital_rest/repositories/UserRepo.java
+++ src/main/java/com/bagas/hospital_rest/repositories/UserRepo.java
@@ -1,6 +1,9 @@
 package com.bagas.hospital_rest.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.bagas.hospital_rest.entity.UserEntity;
 
@@ -8,4 +11,9 @@ import com.bagas.hospital_rest.entity.UserEntity;
 public interface UserRepo extends JpaRepository<UserEntity, Long> {
 
 	UserEntity findByUsername(String username);
+
+	@Query("SELECT DISTINCT u FROM UserEntity u LEFT JOIN FETCH u.userInfo ui LEFT JOIN FETCH u.appointments uapp LEFT JOIN FETCH u.authorities ua")
+	@Override List<UserEntity> findAll();
+	
+	
 }
