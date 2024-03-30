0ea996338ccf30a20c837b291d6c791a0c19da37
Alexandr Bogoslovskiy <balics@mail.ru>
2024-03-30 10:10:39 +0300

    add operations to role such as getAll, getOne, create, delete, update

--- src/main/java/com/bagas/hospital_rest/controller/RoleContoller.java
+++ src/main/java/com/bagas/hospital_rest/controller/RoleContoller.java
@@ -1,15 +1,24 @@
 package com.bagas.hospital_rest.controller;
 
 import java.util.List;
-import java.util.stream.Collectors;
+
+import javax.management.relation.RoleNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.hateoas.EntityModel;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import com.bagas.hospital_rest.entity.RoleEntity;
+import com.bagas.hospital_rest.exceptions.RoleExistsException;
 import com.bagas.hospital_rest.models.Role;
 import com.bagas.hospital_rest.services.RoleService;
 
@@ -19,9 +28,54 @@ public class RoleContoller {
 	@Autowired
 	private RoleService roleService;
 	
+	@GetMapping("/roles")
+	public ResponseEntity<List<Role>> getAllRoles() {
+		return ResponseEntity.ok(roleService.getAll());
+	}
+	
+	@GetMapping("/roles/{id}")
+	public ResponseEntity<Role> getOneRole(@PathVariable("id") Long id) {
+		try {
+			return ResponseEntity.ok(roleService.getOne(id));
+		} catch (RoleNotFoundException e) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Роль не найдена", e);
+		}
+	}
+	
 	@GetMapping("/users/{id}/roles")
 	public ResponseEntity<List<Role>> getRolesByUser(@PathVariable("id") Long userId) {
 		List<Role> roles = roleService.getAllByUser(userId);
 		return ResponseEntity.ok(roles);
 	}
+	
+	@PostMapping("/roles")
+	public ResponseEntity<Role> createRole(@RequestBody RoleEntity roleEntity, UriComponentsBuilder builder) {
+		try {
+			Role role = roleService.create(roleEntity);
+			
+			return ResponseEntity.created(builder.path("/roles/{id}").buildAndExpand(role.getId())
+					.toUri()).body(role);
+		} catch (RoleExistsException e) {
+			throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Роль с таким названием уже существует", e);
+		}
+	}
+	
+	@DeleteMapping("/roles/{id}")
+	public ResponseEntity<Long> deleteRole(@PathVariable("id") Long id) {
+		try {
+			return ResponseEntity.ok(roleService.delete(id));
+		} catch (RoleNotFoundException e) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Роль не найдена", e);
+		}
+	}
+	
+	@PutMapping("/roles/{id}")
+	public ResponseEntity<Role> updateRole(@PathVariable("id") Long id, @RequestBody RoleEntity roleEntity) {
+		try {
+			return ResponseEntity.ok(roleService.update(id, roleEntity));
+		} catch (RoleNotFoundException e) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Роль не найдена", e);
+		}
+	}
+	
 }
--- src/main/java/com/bagas/hospital_rest/services/RoleService.java
+++ src/main/java/com/bagas/hospital_rest/services/RoleService.java
@@ -3,9 +3,13 @@ package com.bagas.hospital_rest.services;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.management.relation.RoleNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.bagas.hospital_rest.entity.RoleEntity;
+import com.bagas.hospital_rest.exceptions.RoleExistsException;
 import com.bagas.hospital_rest.models.Role;
 import com.bagas.hospital_rest.repositories.RoleRepo;
 
@@ -15,7 +19,40 @@ public class RoleService {
 	@Autowired
 	private RoleRepo roleRepo;
 	
+	public List<Role> getAll() {
+		return roleRepo.findAll().stream().map(Role::toModel).collect(Collectors.toList());
+	}
+	
 	public List<Role> getAllByUser(Long userId) {
 		return roleRepo.findAllByUserId(userId).stream().map(Role::toModel).collect(Collectors.toList());
+	}
+	
+	public Role getOne(Long id) throws RoleNotFoundException {
+		RoleEntity role = roleRepo.findById(id)
+				.orElseThrow(() -> new RoleNotFoundException());
+		
+		return Role.toModel(role);
+	}
+	
+	public Role create(RoleEntity roleEntity) throws RoleExistsException {
+		if(roleRepo.findByAuthority(roleEntity.getAuthority()) != null) throw new RoleExistsException();
+		
+		return Role.toModel(roleRepo.save(roleEntity));
+	}
+	
+	public Long delete(Long id) throws RoleNotFoundException {
+		if(roleRepo.findById(id).get() == null) throw new RoleNotFoundException();
+		
+		roleRepo.deleteById(id);
+		return id;
+	}
+	
+	public Role update(Long id, RoleEntity roleEntity) throws RoleNotFoundException {
+		RoleEntity updatingRole = roleRepo.findById(id)
+				.orElseThrow(() -> new RoleNotFoundException());
+		
+		updatingRole.setAuthority(roleEntity.getAuthority());
+		
+		return Role.toModel(roleRepo.save(updatingRole));
 	}
 }
