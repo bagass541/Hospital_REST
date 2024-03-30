265a3c1e95f5caa01d1220d2ef8d402b50691913
Alexandr Bogoslovskiy <balics@mail.ru>
2024-03-30 10:11:58 +0300

    add self, put, delete links

--- src/main/java/com/bagas/hospital_rest/models/Role.java
+++ src/main/java/com/bagas/hospital_rest/models/Role.java
@@ -4,6 +4,7 @@ import org.springframework.hateoas.Link;
 import org.springframework.hateoas.RepresentationModel;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 
+import com.bagas.hospital_rest.controller.RoleContoller;
 import com.bagas.hospital_rest.controller.UserController;
 import com.bagas.hospital_rest.entity.RoleEntity;
 
@@ -19,14 +20,24 @@ public class Role extends RepresentationModel<Role> {
 	private String authority;
 	
 	public static Role toModel(RoleEntity entity) {
+		Long roleId = entity.getId();
 		Role role = new Role();
-		role.setId(entity.getId());
+		role.setId(roleId);
 		role.setAuthority(entity.getAuthority());
+		
+		Link selfLink = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(RoleContoller.class)
+				.getOneRole(roleId)).withSelfRel();
+		
+		Link putLink = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(RoleContoller.class)
+				.updateRole(roleId, entity)).withRel("update").withType("PUT");
+		
+		Link deleteLink = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(RoleContoller.class)
+				.deleteRole(roleId)).withRel("delete").withType("DELETE");
 		
 		Link users = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(UserController.class)
 				.getUsers()).withRel("users");
 		
-		role.add(users);
+		role.add(selfLink, putLink, deleteLink, users);
 		
 		return role;
 	}
