3fe1f5a2ea8292ec545d7cfc6f2567c8c3cba03c
Alexandr Bogoslovskiy <balics@mail.ru>
2024-03-30 10:06:46 +0300

    add new operations to appointment entity such as getOne, create, delete, update, getAll

--- src/main/java/com/bagas/hospital_rest/controller/AppointmentController.java
+++ src/main/java/com/bagas/hospital_rest/controller/AppointmentController.java
@@ -5,22 +5,36 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.EntityModel;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.bagas.hospital_rest.models.DoctorAppointment;
+import com.bagas.hospital_rest.entity.AppointmentEntity;
+import com.bagas.hospital_rest.exceptions.AppointmentNotFoundException;
 import com.bagas.hospital_rest.models.Appointment;
 import com.bagas.hospital_rest.services.AppointmentService;
 
-@Controller
+@RestController
 public class AppointmentController {
 
 	@Autowired
 	private AppointmentService appointmentService;
 	
-	@GetMapping("users/{id}/appointments")
+	@GetMapping("/appointments")
+	public ResponseEntity<List<Appointment>> getAppointments() {
+		return ResponseEntity.ok(appointmentService.getAll());
+	}
+	
+	@GetMapping("/users/{id}/appointments")
 	public ResponseEntity<List<Appointment>> getAppointmentsForUser(@PathVariable("id") Long userId) {
 		List<Appointment> userAppointments = appointmentService.getAllByUser(userId);
 		return ResponseEntity.ok(userAppointments);
@@ -34,5 +48,47 @@ public class AppointmentController {
 					return model;
 				}).collect(Collectors.toList());
 		return ResponseEntity.ok(doctorAppointments);
+	}
+	
+	@GetMapping("/appointments/{id}")
+	public ResponseEntity<Appointment> getOneAppointment(@PathVariable("id") Long id) {
+		try {
+			Appointment appointment = appointmentService.getOne(id);
+			return ResponseEntity.ok(appointment);
+		} catch (AppointmentNotFoundException e) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Запись не найдена", e);
+		}
+	}
+	
+	@PostMapping("/appointments")
+	public ResponseEntity<Appointment> createAppointment(@RequestBody AppointmentEntity appointment, UriComponentsBuilder builder) {
+		try {
+			Appointment newAppointment = appointmentService.create(appointment);
+			
+			return ResponseEntity.created(builder.path("/appointments/{id}")
+					.buildAndExpand(newAppointment.getId())
+					.toUri()).body(newAppointment);
+		} catch (AppointmentNotFoundException e) {
+			throw new ResponseStatusException(HttpStatus.FORBIDDEN, "This appointments exists", e);
+		}
+	}
+	
+	@DeleteMapping("/appointments/{id}")
+	public ResponseEntity<Long> deleteAppointment(@PathVariable("id") Long id) {
+		try {
+			return ResponseEntity.ok(appointmentService.delete(id));
+		} catch (AppointmentNotFoundException e) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Запись не найдена", e);
+		}
+	}
+	
+	@PutMapping("/appointments/{id}")
+	public ResponseEntity<Appointment> updateAppointment(@PathVariable("id") Long id, @RequestBody AppointmentEntity appointment) {
+		try {
+			Appointment newAppointment = appointmentService.update(id, appointment);
+			return ResponseEntity.ok(newAppointment);
+		} catch (AppointmentNotFoundException e) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Запись не найдена", e);
+		}
 	}
 }
