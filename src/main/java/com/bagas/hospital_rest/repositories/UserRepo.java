2c345225d3a75b7ef7c35a508de0cd5487c2ebf1
Alexandr Bogoslovskiy <balics@mail.ru>
2024-03-31 21:53:56 +0300

    create method to get all users by role

--- src/main/java/com/bagas/hospital_rest/repositories/UserRepo.java
+++ src/main/java/com/bagas/hospital_rest/repositories/UserRepo.java
@@ -4,16 +4,19 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.bagas.hospital_rest.entity.UserEntity;
 
 
 public interface UserRepo extends JpaRepository<UserEntity, Long> {
 
-	UserEntity findByUsername(String username);
-
 	@Query("SELECT DISTINCT u FROM UserEntity u LEFT JOIN FETCH u.userInfo ui LEFT JOIN FETCH u.appointments uapp LEFT JOIN FETCH u.roles ur")
 	@Override List<UserEntity> findAll();
 	
+	UserEntity findByUsername(String username);
+
+	@Query("SELECT DISTINCT u FROM UserEntity u LEFT JOIN FETCH u.userInfo ui LEFT JOIN FETCH u.appointments uapp LEFT JOIN FETCH u.roles ur WHERE ur.id = :roleId")
+	List<UserEntity> findAllByRole(@Param("roleId") Long id);
 	
 }
