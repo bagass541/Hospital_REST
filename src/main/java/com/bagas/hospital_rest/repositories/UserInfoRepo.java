712f54c6631b99add063fede257e27e7d00b1c95
Alexandr Bogoslovskiy <balics@mail.ru>
2024-03-31 20:16:18 +0300

    override findAll with fetch joins and add fetch joins to findByUserEntityId

--- src/main/java/com/bagas/hospital_rest/repositories/UserInfoRepo.java
+++ src/main/java/com/bagas/hospital_rest/repositories/UserInfoRepo.java
@@ -5,13 +5,16 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.bagas.hospital_rest.entity.UserInfoEntity;
+
+import java.util.List;
 import java.util.Optional;
-
-
 
 public interface UserInfoRepo extends JpaRepository<UserInfoEntity, Long> {
 
-	@Query("SELECT ui FROM UserInfoEntity ui WHERE ui.user.id = :id")
+	@Query("SELECT ui FROM UserInfoEntity ui LEFT JOIN FETCH ui.user")
+	@Override List<UserInfoEntity> findAll();
+
+	@Query("SELECT DISTINCT ui FROM UserInfoEntity ui LEFT JOIN FETCH ui.user WHERE ui.user.id = :id")
 	Optional<UserInfoEntity> findByUserEntityId(@Param("id") Long userId);
 	
 	UserInfoEntity findByNumber(String number);
